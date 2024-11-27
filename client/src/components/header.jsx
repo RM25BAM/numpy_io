8bb06aca4ad6457eef24351391f4a7ec814ab82c
RM25BAM <nmpiedrabuena2003@gmail.com>
2024-11-26 23:55:18 -0500

    add

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './header.css';
 import logo from '../assets/logo.gif'
-import ReactTypingEffect from 'react-typing-effect';
+
 function Header() {
     return (
         <header className="header">
             <div className="header-left">
                 <div className="logo">
-                    <img src={logo} alt="logo" className="logo-icon" />
+                    {/*  <img src={logo} alt="logo" className="logo-icon" /> */}
                 </div>
             </div>
             <nav className="header-nav">
-                <button className="btn">
+                {/*                 <button className="btn">
                     <a href="#features" className="">Features</a>
                 </button>
                 <button className="btn">
@@ -19,19 +19,21 @@ function Header() {
                 </button>
                 <button className="btn">
                     <a href="#Contact">Contact</a>
-                </button>
+                </button> */}
             </nav>
             <div className="header-right">
-                <button onClick className="signup">
-                    <a href="#signup" className="signup_text">Sign up</a>
-                </button>
                 <button onClick className="signin">
                     <a href="#signin" className="signin_text">Sign in</a>
                 </button>
+                <button onClick className="signup">
+                    <a href="#signup" className="signup_text">Sign up</a>
+                </button>
             </div>
         </header>
+
     );
 };
 
 export default Header;
 
+/* #3b67e1 */
